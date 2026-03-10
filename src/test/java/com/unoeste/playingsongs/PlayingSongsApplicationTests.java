49ccb9c2c752c93696f6cea74f2325e8da5478f8
Danihacker2024 <danielnevs2020@gmail.com>
2026-03-10 10:43:38 -0300

    Primeiro Commit

---
+++ src/test/java/com/unoeste/playingsongs/PlayingSongsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unoeste.playingsongs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayingSongsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
